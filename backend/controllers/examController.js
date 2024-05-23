ff1be429e05b6bf0fef4b94257224e897fddad6c
Dr-AddictStein <dr.addictstein@gmail.com>
2024-05-24 04:38:42 +0600

    exam CRUD done

--- backend/controllers/examController.js
+++ backend/controllers/examController.js
@@ -1,5 +1,23 @@
+import mongoose from "mongoose";
 import examModel from "../models/examModel.js";
 
+export const getAllExams = async (req, res) => {
+  const exams = await examModel.find({});
+  res.status(200).json(exams);
+};
+export const getSingleExam = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid ID.!." });
+  }
+  const exam = await examModel.findById(id);
+
+  if (exam) {
+    res.status(200).json(exam);
+  } else {
+    return res.status(400).json({ error: "No Such Exam Found.!." });
+  }
+};
 export const createExam = async (req, res) => {
   try {
     const newExam = new examModel(req.body);
@@ -7,5 +25,35 @@ export const createExam = async (req, res) => {
     res.status(201).json(savedExam);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateExam = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid ID.!." });
+  }
+  const exam = await examModel.findOneAndUpdate({ _id: id }, { ...req.body });
+
+  if (exam) {
+    const toSend = await examModel.findById(id);
+    res.status(200).json(toSend);
+  } else {
+    return res.status(400).json({ error: "No Such Exam Found.!." });
+  }
+};
+
+export const deleteExam = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid ID.!." });
+  }
+
+  const exam = await examModel.findOneAndDelete({ _id: id });
+
+  if (exam) {
+    res.status(200).json(exam);
+  } else {
+    return res.status(400).json({ error: "No Such exam Found.!." });
   }
 };
--- backend/routes/examRoutes.js
+++ backend/routes/examRoutes.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import { createExam } from '../controllers/examController.js';
+import { createExam, deleteExam, getAllExams, getSingleExam, updateExam } from '../controllers/examController.js';
 
 const router=express.Router();
 
+
+router.get('/',getAllExams);
+router.get('/:id',getSingleExam);
+
+
 router.post('/',createExam);
+router.patch('/:id',updateExam);
+router.delete('/:id',deleteExam);
 
 
 
